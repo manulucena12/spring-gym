b23a33fde784fc74081a9bb7312de0051befdfe3
manulucena <mlucenasalas@gmail.com>
2024-12-20 16:20:32 +0100

    Starting project

---
+++ spring-gym/src/test/java/com/manu/SpringGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
